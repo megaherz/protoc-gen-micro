f78c2be7424f1f148f074f715b46683c73c3ebf0
Andrei Shneider <megaherz@bk.ru>
2018-04-24 22:31:18 +0300

    retag added and registered

--- main.go
+++ main.go
@@ -53,8 +53,9 @@ import (
 	"os"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/micro/protoc-gen-micro/generator"
-	_ "github.com/micro/protoc-gen-micro/plugin/micro"
+	"github.com/megaherz/protoc-gen-micro/generator"
+	_ "github.com/megaherz/protoc-gen-micro/plugin/micro"
+	_ "github.com/megaherz/protoc-gen-micro/retag"
 )
 
 func main() {
